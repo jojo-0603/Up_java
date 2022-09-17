bf9ed9f53e31b861937da801fc902cef7e1c5116
Johan John <coderhacker603@gmail.com>
2022-09-17 09:47:10 +0530

    The commit after Updating_variables

---
+++ Updating_variables.java
@@ -0,0 +1,15 @@
+public class Updating_variables {
+public static void main(String[] args) {
+    
+   int dist_adam=0,dist_lucy=0;
+   System.out.println(dist_adam);
+    System.out.println(dist_lucy);    
+    
+    dist_adam=dist_adam+20;
+    dist_lucy=15;
+    System.out.println(dist_adam);
+    System.out.println(dist_lucy);
+
+}
+
+}
